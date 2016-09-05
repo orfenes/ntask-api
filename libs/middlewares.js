7a8592b1d366aa12b4cddcfd72d023de4b16438f
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-05 12:03:51 -0300

    pagina 63

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,9 @@
 module.exports = app => {
 	app.set('port', 3000);
 	app.set('json spaces', 4);
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    delete req.body.id;
+    next();
+  });
 };
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+  const Users = app.db.models.Users;
+
+  app.get('/users/:id', (req, res) => {
+    Users.findById(req.params.id, {
+      attributes: ["id", "name", "email"]
+    })
+    .then(result => res.json(result))
+    .catch(error => {
+      res.status(412).json({msg: error.message});
+    });
+  });
+
+  app.delete('/users/:id', (req, res) => {
+    Users.destroy({where: {id: req.params.id} })
+    .the(result => sendStatus(204))
+    .catch(error => {
+      res.status(412).json({msg: error.message});
+    });
+  });
+
+};
