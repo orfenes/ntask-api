55606e655cc65a90845ae4da20bd1671cd1d69ec
orfenes <digo_orfenes@hotmail.com>
2016-09-18 00:51:13 -0300

    teste token ok

--- test/routes/token.js
+++ test/routes/token.js
@@ -3,33 +3,66 @@ describe("Routes: Token", () => {
   const Users = app.db.models.Users;
 
   describe("POST /token", () => {
-    
     beforeEach(done => {
       Users
         .destroy({where: {}})
         .then(() => Users.create({
           name: "Jose",
-          email: "jose@mail.com",
+          email: "jose@mail.net",
           password: "12345"
         }))
         .then(done());
+    });
+
+    describe("status 401", () => {
+      it("throws error when password is incorrect", done => {
+        request.post("/token")
+          .send({
+            email: "jose@mail.net",
+            password: "SENHA_ERRADA"
+          })
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+          });
+      });
+
+      it("throws error when email not exist", done => {
+        request.post("/token")
+          .send({
+            email: "EMAIL_ERRADO",
+            password: "SENHA_ERRADA"
+          })
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+          });
+      });
+
+      it("throws error when email and password are blank", done => {
+        request.post("/token")
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+          });
+      });
     });
 
     describe("status 200", () => {
       it("returns authenticated user token", done => {
         request.post("/token")
           .send({
-            email: "jose@mail.com",
+            email: "jose@mail.net",
             password: "12345"
           })
           .expect(200)
-          .end((err, res) => {
-            console.log('teste aqui', res.body); 
+          .end((err, res) => {            
             expect(res.body).to.include.keys("token");
             done(err);
           });
       });
     });
+
 
   });
 });
