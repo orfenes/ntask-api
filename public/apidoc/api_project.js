f5f0745ce2391273e6c93fc1207f9aeeecca72d1
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-26 20:10:06 -0300

    implementando certificado digital

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-09-23T15:05:20.107Z",
+    "time": "2016-09-26T23:08:18.292Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
