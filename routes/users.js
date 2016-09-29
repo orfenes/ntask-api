07bda0b38a738e66672ea303b2703c076d597236
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-29 12:24:41 -0300

    Revert "realizando correcoes"
    This reverts commit b5e449a8433cf2ce864a3a77ad2c98325c9f92ba.

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,32 @@
 module.exports = app => {
   const Users = app.db.models.Users;
 
-  app.get("/users/:id", (req, res) => {
+  app.get('/users/:id', (req, res) => {
     Users.findById(req.params.id, {
       attributes: ["id", "name", "email"]
     })
-    .then(result => res.json(result))
-    .catch(error => {
-      res.status(412).json({msg: error.message});
-    });
+      .then(result => res.json(result))
+      .catch(error => {
+        res.status(412).json({msg: error.message});
+      });
   });
 
-  app.delete("/users/:id", (req, res) => {
+  app.delete('/users/:id', (req, res) => {
     Users.destroy({where: {id: req.params.id} })
-    .then(result => sendStatus(204))
-    .catch(error => {
-      res.status(412).json({msg: error.message});
-    });
+      .the(result => sendStatus(204))
+      .catch(error => {
+        res.status(412).json({msg: error.message});
+      });
   });
 
 
-// pagina
-app.post("/users", (req, res) => {
-  User.create(req.body)
-  .then(result => res.json(result))
-  .catch(error => {
-    res.status(412).json({msg: eroor.message});
+  // pagina
+  app.post('/users', (req, res) => {
+    User.create(req.body)
+      .then(result => res.json(result))
+      .catch(error => {
+        res.status(412).json({msg: eroor.message});
+      });
   });
-});
 
 };
