e04d47f1076bb4149ddbc97443c21656e2926039
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-29 14:54:24 -0300

    estrutura de crud rotas user e tasks ok

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ module.exports = app => {
 
   app.get('/users/:id', (req, res) => {
     Users.findById(req.params.id, {
-      attributes: ["id", "name", "email"]
+      attributes: ['id', 'name', 'email']
     })
       .then(result => res.json(result))
       .catch(error => {
@@ -13,7 +13,7 @@ module.exports = app => {
 
   app.delete('/users/:id', (req, res) => {
     Users.destroy({where: {id: req.params.id} })
-      .the(result => sendStatus(204))
+      .then((result) => sendStatus(204))
       .catch(error => {
         res.status(412).json({msg: error.message});
       });
@@ -22,10 +22,10 @@ module.exports = app => {
 
   // pagina
   app.post('/users', (req, res) => {
-    User.create(req.body)
+    Users.create(req.body)
       .then(result => res.json(result))
       .catch(error => {
-        res.status(412).json({msg: eroor.message});
+        res.status(412).json({msg: error.message});
       });
   });
 
