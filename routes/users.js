3d9f5732dcd467f085eed0d67382bac23bc91a5e
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-29 14:57:25 -0300

    corrigindo lint na linha 17 user.js routes

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,12 @@ module.exports = app => {
       });
   });
 
+  // .then(result => {res.sendStatus(204);})
   app.delete('/users/:id', (req, res) => {
     Users.destroy({where: {id: req.params.id} })
-      .then((result) => sendStatus(204))
+      .then(() => {
+        res.sendStatus(204);
+      })
       .catch(error => {
         res.status(412).json({msg: error.message});
       });
