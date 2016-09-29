a7750eacb3a9381cb8f0af9d095c4cb452a1c29d
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-29 12:18:53 -0300

    Revert "teste token ok"
    This reverts commit 55606e655cc65a90845ae4da20bd1671cd1d69ec.

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,7 @@ module.exports = app => {
       Users.findById(req.user.id, {
         attributes: ["id", "name", "email"]
       })
-      .then(result => {
-        res.json(result);
-      })
+      .then(result => res.json(result))
       .catch(error => {
         res.status(412).json({msg: error.message});
       });
@@ -18,9 +16,7 @@ module.exports = app => {
 
     .delete((req, res) => {
       Users.destroy({where: {id: req.user.id} })
-        .then(result => {
-          res.sendStatus(204);
-        })
+        .then(() => res.sendStatus(204))
         .catch(error => {
           res.status(412).json({msg: error.message});
         });
@@ -28,9 +24,7 @@ module.exports = app => {
 
   app.post("/users", (req, res) => {
     Users.create(req.body)
-      .then(result => {
-        res.json(result);
-      })
+      .then(result => res.json(result))
       .catch(error => {
         res.status(412).json({msg: error.message});
       });
