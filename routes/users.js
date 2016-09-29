e378cbad3cab61853d3802470824979154145896
Rodrigo Castro <rfocastro@sp.r7.com>
2016-09-29 12:23:55 -0300

    Revert "implementando autenticacao"
    This reverts commit e7c5ec49fd9288003f8f9bd2dbb39a613b021a29.

--- routes/users.js
+++ routes/users.js
@@ -5,26 +5,28 @@ module.exports = app => {
     Users.findById(req.params.id, {
       attributes: ["id", "name", "email"]
     })
-      .then(result => res.json(result))
-      .catch(error => {
-        res.status(412).json({msg: error.message});
-      });
+    .then(result => res.json(result))
+    .catch(error => {
+      res.status(412).json({msg: error.message});
+    });
   });
 
   app.delete("/users/:id", (req, res) => {
     Users.destroy({where: {id: req.params.id} })
-      .then(result => res.sendStatus(204))
-      .catch(error => {
-        res.status(412).json({msg: error.message});
-      });
+    .then(result => sendStatus(204))
+    .catch(error => {
+      res.status(412).json({msg: error.message});
+    });
   });
 
-  app.post("/users", (req, res) => {
-    Users.create(req.body)
-      .then(result => res.json(result))
-      .catch(error => {
-        res.status(412).json({msg: error.message});
-      });
+
+// pagina
+app.post("/users", (req, res) => {
+  User.create(req.body)
+  .then(result => res.json(result))
+  .catch(error => {
+    res.status(412).json({msg: eroor.message});
   });
+});
 
 };
